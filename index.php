5e95d4662474b47c7cfafb62534ba14dea7166f7
tabreturn <github@tabreturn.com>
2018-02-08 19:58:18 +1300

    - add hide/show feature

--- index.php
+++ index.php
@@ -44,26 +44,68 @@
 
     <h1>What?</h1>
     <div class="sect_cont">
-      <h2>Welcome!</h2>
       <p class="larger">
-        Et nobis tempore quibusdam non et ab. Enim ab impedit magni nemo
-        perferendis voluptatem. Neque necess itatibus quibusdam quo delectus
-        asperiores eveniet.
+        Open Jam Aotearoa is a 48 hour game jam<sup>*</sup>. Turn up with your
+        own device, hang out with cool peeps, and make an awesome game, even if
+        you have never made a game before but love the idea!
       </p>
       <p>
-        Reiciendis a nesciunt vero consequatur reiciendis corporis vero. Quam
-        voluptas sequi accusamus vitae dignissimos. Ratione quasi asperiores qui
-        ipsa amet. Quia minima nisi iure beatae. Non provident ea voluptatem
-        dignissimos possimus. Est fuga assumenda possimus in suscipit quo
-        perferendis et.
+        There will be enough power points and WiFi available and we will provide
+        snacks (and soup!) each day. We will have a networked harddrive on-site
+        for participants to openly share assets created during the jam with each
+        other. We'd love to see some weird cross-pollination of game art/ code/
+        audio/ etc between games.
       </p>
+      <p>
+        It will be $20 cash on the Friday to participate in the jam (the money
+        will all go to food and paying the poster designer).
+      </p>
+      <p>
+        We as organizers are dedicated to making the jam inclusive of beginners
+        who haven't created a game before and will be pottering around the jam
+        helping with any tech/audio/art etc quandaries that you might have :))
+        You can come along with a pre-made team or we will be happy to
+        facilitate team creation during the event. Also we're happy for solo
+        content creators to come along to contribute assets to many games at
+        once- (this often works super well for audio peeps with specialised
+        skills in sound design/ implementation/ composition).
+      </p>
+
+      <script>
+        function dd(ddshow) {
+          var ddel = document.getElementById(ddshow);
+          if (ddel.style.display == 'none' || !ddel.style.display)
+            ddel.style.display = 'block';
+          else
+            ddel.style.display = 'none';
+        }
+      </script>
+      <div class="hideshow">
+        <dt onclick="dd('gamejam_def')"><sup>*</sup> Erm, what is a game jam?</dt>
+        <dd id="gamejam_def">
+          <p>
+            <em>A game jam is a hackathon for video games. It is a gathering of
+            people for the purpose of planning, designing, and creating one or
+            more games within a short span of time, in our case 48 hours. Open
+            Jam Aotearoa aims to stretch the classical definition of video game
+            development towards more artistic and experimental outputs.
+            Addressing a prevalent lack of diversity in the game industry we
+            plan to create an inclusive environment, inviting participants like
+            artists, designers, musicians, storytellers that don’t necessarily
+            consider themselves programmers, developers or gamers. On Friday
+            evening we will announce the theme of the jam and participants can
+            spend as much time as they want during the opening hours to work on
+            their game(s), either in a group or by themselves, if they
+            prefer.</em>
+          <p>
+        </dd>
+      </div>
+
       <div class="sponsors">
         <h2>Sponsors</h2>
-        <img src="img/sponsor-trump.png" alt="trump pence" />
-        <img src="img/sponsor-monsanto.png" alt="monsanto" />
         <img src="img/sponsor-massey.png" alt="massey university" />
-        <img src="img/sponsor-trump.png" alt="trump pence" />
       </div>
+
     </div>
 
   </div>
